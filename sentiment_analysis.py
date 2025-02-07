05944da3a0dc6aefcf52e1b5dfc1af8f1e62929a
engleong <engleong.lee@gbgplc.com>
2025-02-07 16:51:15 +0800

    score more than 0.9

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -52,7 +52,7 @@ def check_news(start_date, end_date, keywords):
             score = result[0]['score']
 
             # Append the news item to the list
-            if (sentiment == 'positive' or sentiment == 'negative'):
+            if (sentiment == 'positive' or sentiment == 'negative') and score > 0.9:
                 news_items.append({
                     "title": title,
                     "description": description,
